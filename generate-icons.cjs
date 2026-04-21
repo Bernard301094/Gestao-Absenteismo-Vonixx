37deaa5e42fb3c601a0a44e991f885558bbe722a
Bernard De Freitas <114589760+Bernard301094@users.noreply.github.com>
2026-04-20 22:48:38 -0300

    fix: usa icons/icon-512.webp como fuente para generar todos los tamaños

--- generate-icons.cjs
+++ generate-icons.cjs
@@ -2,7 +2,7 @@ const sharp = require('sharp');
 const sizes = [48, 72, 96, 128, 192, 256, 512];
 
 sizes.forEach(size => {
-  sharp('icons/icon.png')
+  sharp('icons/icon-512.webp')
     .resize(size, size)
     .webp()
     .toFile(`icons/icon-${size}.webp`, (err) => {
